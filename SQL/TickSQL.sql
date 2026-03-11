4826b3100fa275b8cdfc9cad3c11a7e483cde27f
Rachel Kirtland <rachelkirtland409@gmail.com>
2026-03-11 20:13:46 +0000

    Slight change to sql (Complete)

--- SQL/TickSQL.sql
+++ SQL/TickSQL.sql
@@ -3,9 +3,6 @@ select * from location;
 select * from sighting;
 select * from species;
 select * from login;
-
--- ALTER TABLE importeddata MODIFY COLUMN id VARCHAR(255);
--- ALTER TABLE importeddata CHANGE COLUMN `date` `date_time` TEXT;
 -- DROP TABLE sighting;
 -- DROP TABLE species;
 -- DROP TABLE location;
@@ -19,15 +16,21 @@ INSERT IGNORE INTO location (locationName)
 select location
 from importeddata;
 
--- Inserting into sighting where id is correct length using id's instead of names
+-- ALTER TABLE importeddata MODIFY COLUMN id VARCHAR(255);
+-- ALTER TABLE importeddata CHANGE COLUMN `date` `date_time` TEXT;
+
+-- Inserting into sighting where id is correct length and date/time is valid - using id's instead of names
 INSERT INTO sighting(sightingID,speciesID,locationID,date_time)
 SELECT i.id,s.speciesID,l.locationID,i.date_time
 FROM importeddata i 
 JOIN species s on i.species = s.speciesName
 join location l on i.location = l.locationName
-WHERE (LENGTH(i.id) = 20);
+WHERE (LENGTH(i.id) = 20) AND STR_TO_DATE(i.date_time, '%Y-%m-%dT%H:%i:%s') IS NOT NULL;
 
 -- Data that wasnt entered into sighting due to errors in data
--- shows all but date_time errors
 SELECT * FROM importeddata i
 WHERE NOT EXISTS(SELECT 1 FROM sighting s WHERE s.sightingID = i.id);
+
+-- Select all Where date_time is not valid
+SELECT * FROM importeddata
+WHERE STR_TO_DATE(date_time, '%Y-%m-%dT%H:%i:%s') IS NULL;
