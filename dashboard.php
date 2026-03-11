3b3e0d749d574f6f32d85d88ebab38da79f844c8
Rachel Kirtland <rachelkirtland409@gmail.com>
2026-03-11 16:01:47 +0000

    Created insights page and connected to dashboard

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Dashboard" content="Main Dashboard">
     <title>Tick Visualiser</title>
     <link rel="stylesheet" href="style.css">
     <!-- chart.js for plotting the trend graphs -->
@@ -24,9 +24,9 @@
             </button>
 
             <div class="top-menu">
-                <a href="#">Home</a>
+                <a href="dashboard.php">Home</a>
                 <a href="#">Browse Data</a>
-                <a href="#">Insights</a>
+                <a href="insights.php">Insights</a>
                 <a href="#">Upload</a>
                 <a href="#">Contact</a>
             </div>
---
+++ insights.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="Insights" content="Tick Insights">
+    <title>Tick Visualiser</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- chart.js for plotting the trend graphs -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="app.js"></script>
+</head>
+
+<body>
+
+    <!-- top navigation -->
+    <header class="top-nav">
+        <div class="nav-container">
+            <div class="logo-title">Tick Visualiser</div>
+
+            <!-- default hidden on large screens, only appears on mobile screens -->
+            <button class="menu-toggle" aria-label="Toggle Menu">
+                ☰
+            </button>
+
+            <div class="top-menu">
+                <a href="dashboard.php">Home</a>
+                <a href="#">Browse Data</a>
+                <a href="insights.php">Insights</a>
+                <a href="#">Upload</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+    </header>
+
+    <!-- banner -->
+    <section class="banner-section">
+        <div class="content-container">
+            <!-- <h1> General insights for understanding ticks in the UK</h1> -->
+        </div>
+        <h1> General insights for understanding ticks in the UK</h1>
+    </section>
+
+    <!-- main dashboard -->
+    <main class="dashboard-container">
+
+        <div class="dashboard-grid">
+
+        </div>
+
+    </main>
+
+</body>
+
+</html>
