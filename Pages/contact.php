a91d191b9d56390629c317d28c6f040f5cc745d6
Siaiesaef <siapugal@yahoo.com>
2026-03-16 15:58:39 +0000

    partially done register

--- Pages/contact.php
+++ Pages/contact.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Contact Us • Tick Visualiser</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/contact.css">
 </head>
 
 <body>
