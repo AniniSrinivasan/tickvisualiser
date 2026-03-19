5d782d02122f4ff6348fb0201612406528897adc
Rachel Kirtland <rachelkirtland409@gmail.com>
2026-03-19 10:24:53 +0000

    AGAIN db changes

--- SQL/TickSQL.sql
+++ SQL/TickSQL.sql
@@ -19,14 +19,19 @@ INSERT IGNORE INTO location (location_name)
 SELECT location
 FROM imported_data;
 
+insert into upload(upload_id,upload_name,upload_date)
+values ("1","imported_data","2026-02-20");
+
 -- INSERTING INTO sighting WHERE id IS CORRECT LENGTH AND date_time IS VALID
-INSERT INTO sighting (sighting_id, species_id, location_id, date_time, upload_id)
-SELECT i.id, s.species_id, l.location_id, i.date_time, 1
+INSERT INTO sighting  (species_id, location_id, date_time, upload_id)
+SELECT s.species_id, l.location_id, i.date_time, 1
 FROM imported_data i
 JOIN species s ON i.species = s.species_name
 JOIN location l ON i.location = l.location_name
 WHERE (LENGTH(i.id) = 20) 
 AND STR_TO_DATE(i.date_time, '%Y-%m-%dT%H:%i:%s') IS NOT NULL;
+
+-- drop table sighting;
 
 -- DATA THAT WASN'T ENTERED INTO sighting DUE TO ERRORS
 SELECT * FROM imported_data i
