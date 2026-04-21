f37cc12779b9b9cb29043d3efae6baabccc87d01
Siaiesaef <siapugal@yahoo.com>
2026-04-21 21:36:11 +0100

    fixed accessibility on dashboard and seo issue on manage user

--- Pages/manage-user.php
+++ Pages/manage-user.php
@@ -11,6 +11,7 @@ include_once('../functions/db_connect.php'); ?>
 
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Manage User page">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/style.css">
     <title>Total-Sightings • Tick Visualiser</title>
